593a515e361bad6800181d4ba54c8f380bfea49d
SinduAditya <77913475+Sinduaditya@users.noreply.github.com>
2024-11-09 13:46:33 +0000

    estimasi-mobilv1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
+import pickle
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+model = pickle.load(open('estimasi_mobil.sav','rb'))
+
+st.title('Estimasi Harga Mobil Bekas')
+
+year = st.number_input('Input Tahun Mobil')
+mileage = st.number_input('Input KM Mobil')
+tax = st.number_input('Input Pajak Mobil')
+mpg = st.number_input('Input Konsumsi BBM')
+engineSize = st.number_input('Input Engine Size')
+
+predict = ''
+
+if st.button('Estimasi Harga') :
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engineSize]]
+    )
+    st.write('Estimasi harga Mobil Bekas dalam Ponds : ', predict)
+    st.write('Estimasi harga Mobil Bekas dalam IDR (Juta) : ' , predict*20000)
+    
